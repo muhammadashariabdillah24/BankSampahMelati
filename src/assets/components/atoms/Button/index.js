c93e187c7ca7ff7a63e845a5b3da4eda7d3da26f
muhammadashariabdillah24 <tutordunet09@gmail.com>
2022-07-30 17:06:20 +0700

    Create button component with atomic design concept

--- src/assets/components/atoms/Button/index.js
+++ src/assets/components/atoms/Button/index.js
@@ -1,24 +1,54 @@
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 
 export const Button = ({
   outline,
-  ContainerStyle,
+  buttonOutlineStyle,
+  buttonStyle,
   onPress,
-  WrapperIconAndTextStyle,
   children,
 }) => {
   return outline ? (
-    <TouchableOpacity style={ContainerStyle} onPress={onPress}>
-      <View style={WrapperIconAndTextStyle}>{children}</View>
+    <TouchableOpacity style={[styles.buttonOutlineStyle, buttonOutlineStyle]} onPress={onPress}>
+      <View style={styles.wrapperIconAndText}>{children}</View>
     </TouchableOpacity>
   ) : (
-    <TouchableOpacity style={ContainerStyle} onPress={onPress}>
-      <View style={WrapperIconAndTextStyle}>{children}</View>
+    <TouchableOpacity style={[styles.buttonStyle, buttonStyle]} onPress={onPress}>
+      <View style={styles.wrapperIconAndText}>{children}</View>
     </TouchableOpacity>
   );
 };
 
 export const ButtonPrinter = ({ContainerStyle, children}) => {
-  return <View style={ContainerStyle}>{children}</View>;
+  return (
+    <View style={ContainerStyle}>{children}</View>
+  )
 };
+
+
+const styles = StyleSheet.create({
+  buttonOutlineStyle: {
+    marginHorizontal: 20,
+    borderRadius: 5,
+    borderColor: '#fff',
+    borderWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: 55,
+  },
+  buttonStyle: {
+    marginHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: '#188BCC',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: 55,
+  },
+  wrapperIconAndText: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
